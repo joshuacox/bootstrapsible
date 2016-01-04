d1a339d64ee1ecbf33455650a155ef89a327f685
Josh Cox <josh@webhosting.coop>
2016-01-04 08:46:16 -0600

    submodule

--- bootstrapsible.sh
+++ bootstrapsible.sh
@@ -5,4 +5,5 @@ cd ~/git
 git clone https://github.com/ansible/ansible.git
 cd ansible
 git pull
+git submodule update --init --recursive
 echo 'source ~/git/ansible/hacking/env-setup'>>~/.bash_profile
